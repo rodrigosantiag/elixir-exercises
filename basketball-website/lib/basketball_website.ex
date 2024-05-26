a2cf6f8e6153164ded7842f929bba2a94605442e
Rodrigo Santiago <rodrigosantiag@gmail.com>
2024-05-26 07:56:18 -0300

    Add Basketball Website exercise

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,9 +1,16 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    do_extract_from_path(data, String.split(path, "."))
+  end
+
+  defp do_extract_from_path(data, [head | []]), do: data[head]
+
+  defp do_extract_from_path(data, [head | tail]) do
+    do_extract_from_path(data[head], tail)
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    keys = String.split(path, ".")
+    get_in(data, keys)
   end
 end
