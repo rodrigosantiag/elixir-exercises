dbb83a2735cefdaf1ea4e47b65e4bbbad9b5699c
Rodrigo Santiago <rodrigosantiag@gmail.com>
2024-05-30 15:02:42 -0300

    Code improvement for file sniffer exercise

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,36 +1,36 @@
 defmodule FileSniffer do
-  @file_type_translation %{
-    "exe" => "application/octet-stream",
-    "bmp" => "image/bmp",
-    "png" => "image/png",
-    "jpg" => "image/jpg",
-    "gif" => "image/gif"
-  }
+  @media_type_exe "application/octet-stream"
+  @media_type_bmp "image/bmp"
+  @media_type_png "image/png"
+  @media_type_jpg "image/jpg"
+  @media_type_gif "image/gif"
+  @error_message "Warning, file format and file extension do not match."
 
-  @spec type_from_extension(String.t()) :: String.t()
-  def type_from_extension(extension) do
-    @file_type_translation[extension]
-  end
+  @spec type_from_extension(String.t()) :: String.t() | nil
+  def type_from_extension("exe"), do: @media_type_exe
+  def type_from_extension("bmp"), do: @media_type_bmp
+  def type_from_extension("png"), do: @media_type_png
+  def type_from_extension("jpg"), do: @media_type_jpg
+  def type_from_extension("gif"), do: @media_type_gif
+  def type_from_extension(_), do: nil
 
-  @spec type_from_binary(binary()) :: String.t()
+  @spec type_from_binary(binary()) :: String.t() | nil
   def type_from_binary(binary_file) do
     case binary_file do
-      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> "application/octet-stream"
-      <<0x42, 0x4D, _::binary>> -> "image/bmp"
-      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> "image/png"
-      <<0xFF, 0xD8, 0xFF, _::binary>> -> "image/jpg"
-      <<0x47, 0x49, 0x46, _::binary>> -> "image/gif"
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> @media_type_exe
+      <<0x42, 0x4D, _::binary>> -> @media_type_bmp
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> @media_type_png
+      <<0xFF, 0xD8, 0xFF, _::binary>> -> @media_type_jpg
+      <<0x47, 0x49, 0x46, _::binary>> -> @media_type_gif
       _ -> nil
     end
   end
 
-  @spec verify(binary(), String.t()) :: tuple()
-  def verify(file_binary, extension) do
-    if type_from_binary(file_binary) == type_from_extension(extension) and
-         type_from_extension(extension) != nil do
-      {:ok, @file_type_translation[extension]}
-    else
-      {:error, "Warning, file format and file extension do not match."}
-    end
-  end
+  @spec verify(binary(), String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def verify(file_binary, extension),
+    do: do_verify(type_from_binary(file_binary), type_from_extension(extension))
+
+  defp do_verify(nil, nil), do: {:error, @error_message}
+  defp do_verify(type, type), do: {:ok, type}
+  defp do_verify(_type, _another_type), do: {:error, @error_message}
 end
